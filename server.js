d64b954a4f35f50a1bf74c2142d6295481488e3a
Raine Wallace <rainebeaudubs@gmail.com>
2019-02-21 11:36:22 -0500

    I got a start on my custom middleware, but it's having some issues.

--- server.js
+++ server.js
@@ -5,7 +5,17 @@ const userRouter = require('./user-router');
 
 const server = express();
 
+// function upperCase (req, res, next) {
+//   const user = req.body.name;
+//   req.body.name = user.toLowerCase()
+//   .split('')
+//   .map((foo) => foo.charAt(0).toUpperCase() + foo.substring(1))
+//   .join('');
+//   next();
+// };
+
 server.use(express.json());
+// server.use(upperCase);
 
 server.use('/api/posts', postRouter);
 server.use('/api/users', userRouter);
